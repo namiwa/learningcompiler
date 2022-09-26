c5d598fc3b751a25157f23d10cc7f262557004ba
Khairul Iman <42546125+namiwa@users.noreply.github.com>
2022-09-27 00:37:49 +0800

    untested rust compiler

--- rustcompc/src/lexer.rs
+++ rustcompc/src/lexer.rs
@@ -1,3 +1,26 @@
+use std::{fmt, default};
+
+
+// Define our error types. These may be customized for our error handling cases.
+// Now we will be able to write our own errors, defer to an underlying error
+// implementation, or do something in between.
+#[derive(Debug, Clone)]
+struct LexerError {
+  msg: String
+}
+
+type Result<'a, T> = std::result::Result<T, LexerError>;
+
+// Generation of an error is completely separate from how it is displayed.
+// There's no need to be concerned about cluttering complex logic with the display style.
+//
+// Note that we don't store any extra info about the errors. This means we can't state
+// which string failed to parse without modifying our types to carry that information.
+impl fmt::Display for LexerError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      write!(f, "Lexer Error: invalid token {}", self.msg)
+  }
+}
 
 pub struct Lexer {
   source: String,
@@ -14,7 +37,7 @@ impl Lexer {
     }
   }
 
-  pub fn next_char(&mut self) {
+  pub fn next_char(&mut self) -> char {
     if self.cur_pos >= self.source.len() {
       self.curr_char = '\0';
     } else {
@@ -22,6 +45,7 @@ impl Lexer {
       let byte = self.source.as_bytes()[self.cur_pos];
       self.curr_char = byte as char;
     }
+    self.curr_char
   }
 
   pub fn peek(&mut self) -> char {
@@ -33,28 +57,101 @@ impl Lexer {
     self.curr_char = byte as char;
     return self.curr_char
   }
+  
+  fn skip_whitespace(&mut self) {
+    while self.curr_char == ' ' || self.curr_char == '\t' || self.curr_char == '\r' {
+      self.next_char();
+    }
+  }
 
-  pub fn get_token(&mut self) -> char {
-    self.peek();
+  fn skip_comments(&mut self) {
+    if self.curr_char == '#' {
+      while self.curr_char != '\n' {
+        self.next_char();
+      }
+    }
+  }
+
+  pub fn get_token(&mut self) -> Token {
+    self.skip_comments();
+    self.skip_whitespace();
+    let token: Result<Token> = match self.next_char() {
+      '+' => Ok(Token { text: self.curr_char.to_string(), kind: TokenType::PLUS }),
+      '-' => Ok(Token { text: self.curr_char.to_string(), kind: TokenType::MINUS }),
+      '*' => Ok(Token { text: self.curr_char.to_string(), kind: TokenType::ASTERISK }),
+      '/' => Ok(Token { text: self.curr_char.to_string(), kind: TokenType::SLASH }),
+      '\n' => Ok(Token { text: self.curr_char.to_string(), kind: TokenType::NEWLINE }),
+      '\0' => Ok(Token { text: self.curr_char.to_string(), kind: TokenType::EOF }),
+      '=' => {
+        match self.peek() {
+            '=' => {
+              self.next_char();
+              Ok(Token { text: self.curr_char.to_string(), kind: TokenType::EQEQ})
+            }
+            _ => Ok(Token { text: self.curr_char.to_string(), kind: TokenType::EQ }),
+        }
+      },
+      '>' => {
+        match self.peek() {
+            '=' => {
+              self.next_char();
+              Ok(Token { text: self.curr_char.to_string(), kind: TokenType::GTEQ})
+            }
+            _ => Ok(Token { text: self.curr_char.to_string(), kind: TokenType::GT }),
+        }
+      },
+      '<' => {
+        match self.peek() {
+            '=' => {
+              self.next_char();
+              Ok(Token { text: self.curr_char.to_string(), kind: TokenType::LTEQ})
+            }
+            _ => Ok(Token { text: self.curr_char.to_string(), kind: TokenType::LT }),
+        }
+      },
+      '!' => {
+        match self.peek() {
+            '=' => {
+              self.next_char();
+              Ok(Token { text: self.curr_char.to_string(), kind: TokenType::NOTEQ})
+            }
+            _ => Err(LexerError { msg: String::from("! not valid without =") }),
+        }
+      },
+      '\"' => {
+        self.next_char();
+        let start_pos = self.cur_pos;
+
+        while self.curr_char != '\"' {
+          let val = match self.curr_char {
+            '\r' => Err(LexerError { msg: String::from("invalid tab character")}),
+            '\n' => Err(LexerError { msg: String::from("invalid charridge character")}),
+            '\t' => Err(LexerError { msg: String::from("invalid tab character")}),
+            '\\' => Err(LexerError { msg: String::from("invalid slash character")}),
+            '%' => Err(LexerError { msg: String::from("invalid percentage character")}),
+            _default => {
+              self.next_char();
+              Ok(self.curr_char)
+            },
+          };
+          val.unwrap();
+        };
+        let tok_text = self.source.get_mut(start_pos..self.cur_pos).unwrap();
+        Ok(Token { text: String::from(tok_text), kind: TokenType::STRING })
+      }
+      _ => Err(LexerError { msg: String::from("unknown token") }),
+    };
     self.next_char();
-    self.curr_char
+    token.unwrap()
   } 
 }
 
 pub struct Token {
-  pub text: char,
-  pub kind: char,
+  pub text: String,
+  pub kind: TokenType,
 }
 
-impl Token {
-  pub fn new (mut self, token_text: char , token_kind: char) {
-    self.text = token_text;
-    self.kind = token_kind;
-  }
-}
-
-
-enum TokenType {
+pub enum TokenType {
   EOF = -1,
 	NEWLINE = 0,
 	NUMBER = 1,
