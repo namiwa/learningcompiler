54fa20eecacd3d2be9f42a8ee3caf8112853af50
Khairul Iman <42546125+namiwa@users.noreply.github.com>
2023-04-02 03:33:18 +0800

    init: working window in opengl 3.3, glad and glfw

--- cpp-game-engine/build.sh
+++ cpp-game-engine/build.sh
@@ -9,7 +9,7 @@ fi
 
 # build game  
 echo "Compling Game"
-clang++ --std=c++17 -fdiagnostics-color=always -Wall -g -I./dependacies/include -L./dependacies/library ./src/*.cpp ./dependacies/library/libglfw.3.3.dylib -o game_app -framework OpenGl -framework Cocoa -framework IOKit -framework CoreVideo -framework CoreFoundation -Wno-deprecated
+clang++ -v --std=c++17 -fdiagnostics-color=always -Wall -g -I./dependacies/include -L./dependacies/library ./src/*.c ./src/*.cpp ./dependacies/library/libglfw.3.3.dylib -o game_app -framework OpenGl -framework Cocoa -framework IOKit -framework CoreVideo -framework CoreFoundation -Wno-deprecated
 
 # run game
 echo "Running Game, Check window"
--- cpp-game-engine/src/main.cpp
+++ cpp-game-engine/src/main.cpp
@@ -1,3 +1,4 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -8,14 +9,20 @@ int main(void)
 {
     GLFWwindow* window;
     /* init library*/
-    if (!glfwInit()) return -1;
+    auto glfwInt = glfwInit();
+    if (!glfwInt) return -1;
     /* create windowed mode window and its OpenGL context*/
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     window = glfwCreateWindow(640, 480, "Hello World!", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+    gladLoadGL();
     while(!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
