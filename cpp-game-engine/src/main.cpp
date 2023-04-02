45779666211bfce36d6581f779a315ea4a3a6d20
Khairul Iman <42546125+namiwa@users.noreply.github.com>
2023-04-02 18:49:20 +0800

    build: chap 1 for opengl

--- cpp-game-engine/src/main.cpp
+++ cpp-game-engine/src/main.cpp
@@ -1,6 +1,12 @@
+#include <OpenGL/OpenGL.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+} 
+
 
 /**
  * within the hour, before i sleep, create a window in C++.
@@ -10,19 +16,30 @@ int main(void)
     GLFWwindow* window;
     /* init library*/
     auto glfwInt = glfwInit();
-    if (!glfwInt) return -1;
+    if (!glfwInt) {
+        std::cout << "Failed to init GLFW" << std::endl;
+        return -1;
+    }
     /* create windowed mode window and its OpenGL context*/
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    window = glfwCreateWindow(640, 480, "Hello World!", nullptr, nullptr);
+    window = glfwCreateWindow(800, 600, "Hello World!", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
+        std::cout << "Failed to init GLFW window" << std::endl;
         return -1;
     }
     glfwMakeContextCurrent(window);
-    gladLoadGL();
+    int glStatus = gladLoadGL();
+    glViewport(0, 0, 800, 600);
+    if (!glStatus) {
+        glfwTerminate();
+        std::cout << "Failed to init GLAD OpenGL" << std::endl;
+        return -1;
+    }
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     while(!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
