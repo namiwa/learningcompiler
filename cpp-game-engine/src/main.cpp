a3591b648f9e0c1f495db492b2943beaf93d9168
Khairul Iman <42546125+namiwa@users.noreply.github.com>
2023-04-05 03:32:45 +0800

    chap 1: done

--- cpp-game-engine/src/main.cpp
+++ cpp-game-engine/src/main.cpp
@@ -7,6 +7,12 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 } 
 
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 
 /**
  * within the hour, before i sleep, create a window in C++.
@@ -41,7 +47,11 @@ int main(void)
     }
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     while(!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
