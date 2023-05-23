cfcefd659a76c60f1e2ce6181013e86af9adabb7
namiwa <khairul.nasri895@gmail.com>
2023-05-24 02:53:52 +0800

    error: windows build fails to find opengl header on wsl2 (ubuntu)

--- cpp-game-engine/build_win.sh
+++ cpp-game-engine/build_win.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # check if clang++ is installed
-if ! command -v clang++ &> /dev/null
+if ! command -v g++ &> /dev/null
 then
-    echo "clang++ could not be found"
+    echo "g++ could not be found"
     exit
 fi
 
@@ -15,7 +15,7 @@ fi
 
 # build game  
 echo "Compling Game"
-clang++ -v --std=c++17 -fdiagnostics-color=always -Wall -g -I./dependacies/include -L./dependacies/library ./src/*.c ./src/*.cpp ./dependacies/library/libglfw.3.3.dylib -o game_app -ldl -lglfw -Wno-deprecated
+g++ -v --std=c++17 -fdiagnostics-color=always -Wall -g -I./dependacies/include -L./dependacies/library ./src/*.c ./src/*.cpp ./dependacies/library/libglfw.3.3.dylib -o game_app -lOpenGL -Wno-deprecated
 
 # run game
 echo "Running Game, Check window"
