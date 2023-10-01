c14067d2b5410b915bdc133115114a84a46ce8b4
Khairul Iman <42546125+namiwa@users.noreply.github.com>
2023-10-01 17:16:50 +0800

    fix: working make command & starting c VM

---
+++ clox/README.md
@@ -0,0 +1,6 @@
+# CLOX: CraftingInterpreters C Lox VM
+
+Following the book from https://craftinginterpreters.com/a-bytecode-virtual-machine.html
+
+Also learning make from https://makefiletutorial.com
+
--- clox/chunk.h
+++ clox/chunk.h
@@ -2,20 +2,25 @@
 #define clox_chunk_h
 
 #include "common.h"
-
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
+    OP_NEGATE,
     OP_RETURN,
 } OpCode;
 
 typedef struct {
     int count;
     int capacity;
+    ValueArray constants;
     uint8_t* code;
+    int* lines;
 } Chunk;
 
 void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 void freeChunk(Chunk* chunk);
 
 #endif
